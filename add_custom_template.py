e8dc5666fecddeacb785a14cf3406ae0b727dc8e
hllelli2 <hllelli2@liverpool.ac.uk>
2024-11-26 10:48:27 +0000

    Refactor add_custom_template.py to streamline custom template processing and improve argument parsing

--- add_custom_template.py
+++ add_custom_template.py
@@ -1,125 +1,47 @@
 #!/usr/bin/env python
 
 import os
-import time
 import json
 from io import StringIO
 
-import Bio.PDB
-from Bio import pairwise2
+from af3_script_utils import (
+    custom_template_argpase_util,
+    get_custom_template,
+)
 
 
-def get_mmcif(cif, pdb_id, chain_id, start, end):
+def run_custom_template(
+    input_json,
+    target_id,
+    custom_template,
+    custom_template_chain,
+    output_json=None,
+    to_file=True,
+):
+    af3_json = json.load(open(input_json))
+    if not os.path.exists(custom_template):
+        msg = f"Custom template file {custom_template} not found"
+        raise FileNotFoundError(msg)
 
-    parser = Bio.PDB.MMCIFParser(QUIET=True)
-    structure = parser.get_structure(pdb_id, cif)
+    for sequence in af3_json["sequences"]:
+        if not "protein" in sequence:
+            continue
 
-    # Extract release date from the CIF file
-    mmcif_dict = parser._mmcif_dict
-    headers_to_keep = [
-        "_entry.id",
-        "_entry.title",
-        "_entry.deposition_date",
-        "_pdbx_audit_revision_history.revision_date",
-    ]
-    filtered_metadata = {
-        key: mmcif_dict[key] for key in headers_to_keep if key in mmcif_dict
-    }
-
-    # Make metadata if missing
-    if "_pdbx_audit_revision_history.revision_date" not in filtered_metadata:
-        filtered_metadata["_pdbx_audit_revision_history.revision_date"] = time.strftime(
-            "%Y-%m-%d"
+        sequence = get_custom_template(
+            sequence,
+            target_id,
+            custom_template,
+            custom_template_chain,
         )
 
-    for model in structure:
-        chain_to_del = []
-        for chain in model:
-            if chain.id != chain_id:
-                chain_to_del.append(chain.id)
-                continue
+    if to_file:
+        if not output_json:
+            output_json = input_json
 
-        for unwanted_chain in chain_to_del:
-            model.detach_child(unwanted_chain)
+        with open(output_json, "w") as f:
+            json.dump(af3_json, f)
 
-        for chain in model:
-            res_to_del = []
-            for i, res in enumerate(chain):
-                rel_pos = i + 1
-                if rel_pos < start or rel_pos > end or res.id[0] != " ":
-                    res_to_del.append(res)
-
-            for res in res_to_del:
-                chain.detach_child(res.id)
-
-    # Save the filtered structure to a new CIF file
-    io = Bio.PDB.MMCIFIO()
-    io.set_structure(structure)
-    filtered_output = f"{pdb_id}_{chain_id}.cif"
-    io.save(filtered_output)
-
-    # Parse the filtered structure to get the modified MMCIF with no metadata
-    structure = parser.get_structure(pdb_id, filtered_output)
-    mmcif_dict = parser._mmcif_dict
-
-    # Add the filtered metadata to the MMCIF dictionary
-    mmcif_dict.update(filtered_metadata)
-
-    # Save the modified MMCIF with wanted metadata to a string
-    string_io = StringIO()
-    io.set_dict(mmcif_dict)
-    io.save(string_io)
-
-    os.unlink(filtered_output)
-
-    return string_io.getvalue()
-
-
-def check_chains(mmcif_file):
-    parser = Bio.PDB.MMCIFParser(QUIET=True)
-    structure = parser.get_structure("template", mmcif_file)
-    chains = []
-    for model in structure:
-        for chain in model:
-            chains.append(chain.id)
-    return chains
-
-
-def extract_sequence_from_mmcif(mmcif_file):
-    parser = Bio.PDB.MMCIFParser(QUIET=True)
-    structure = parser.get_structure("template", mmcif_file)
-    sequence = ""
-    for model in structure:
-        for chain in model:  # Assuming one chain only
-            for residue in chain:
-                if residue.id[0] == " ":  # Exclude heteroatoms
-                    sequence += residue.resname[
-                        0
-                    ]  # Simplified to take the first letter
-    return sequence
-
-
-def align_and_map(query_seq, template_seq):
-    # Perform pairwise alignment
-    alignments = pairwise2.align.globalxx(query_seq, template_seq)
-    alignment = alignments[0]  # Take the best alignment
-    query_aligned, template_aligned, _, _, _ = alignment
-
-    # Map indices
-    query_indices = []
-    template_indices = []
-
-    query_pos, template_pos = -1, -1
-    for q_char, t_char in zip(query_aligned, template_aligned):
-        if q_char != "-":  # Not a gap in query
-            query_pos += 1
-            if t_char != "-":  # Not a gap in template
-                query_indices.append(query_pos)
-                template_indices.append(template_pos + 1)  # 1-based index for template
-        elif t_char != "-":  # Increment template position for gaps in query
-            template_pos += 1
-
-    return query_indices, template_indices
+    return af3_json
 
 
 if __name__ == "__main__":
@@ -131,95 +53,15 @@ if __name__ == "__main__":
 
     parser.add_argument("--input_json", help="Input alphafold3 json file")
     parser.add_argument("--output_json", help="Output alphafold3 json file")
-    parser.add_argument("--target_id", help="Target id relating to the custom template")
-    parser.add_argument(
-        "--custom_template", help="Custom template to include in the output json"
-    )
-    parser.add_argument(
-        "--custom_template_chain",
-        help="Custom template chain to include in the output json",
-    )
+    parser = custom_template_argpase_util(parser)
 
     args = parser.parse_args()
 
-    af3_json = json.load(open(args.input_json))
-
-    if (
-        len([x for x in af3_json["sequences"] if "protein" in x.keys()]) > 1
-        and not args.target_id
-    ):
-        raise ValueError(
-            "Multiple sequences found in input json. Please specify target id so that custom template can be added to the correct sequence"
-        )
-
-    for sequence in af3_json["sequences"]:
-        if "protein" in sequence:
-
-            # Keep existing templates if they're present
-            if "templates" not in sequence["protein"]:
-                templates = []
-            else:
-                templates = sequence["protein"]["templates"]
-
-            input_sequence = sequence["protein"]["sequence"]
-            seq_id = sequence["protein"]["id"]
-            if isinstance(seq_id, list):
-                if args.target_id and args.target_id not in seq_id:
-                    continue
-            if isinstance(seq_id, str):
-                if args.target_id and args.target_id != seq_id:
-                    continue
-            if not os.path.exists(args.custom_template):
-                raise FileNotFoundError(
-                    f"Custom template file {args.custom_template} not found"
-                )
-
-            chain_info = check_chains(args.custom_template)
-            if len(chain_info) != 1 and not args.custom_template_chain:
-                raise ValueError(
-                    f"Custom template file {args.custom_template} contains {len(chain_info)} chains. Please specify the chain to use with --custom_template_chain"
-                )
-
-            if (
-                args.custom_template_chain
-                and args.custom_template_chain not in chain_info
-            ):
-                raise ValueError(
-                    f"Custom template file {args.custom_template} does not contain chain {args.custom_template_chain}"
-                )
-
-            if not args.custom_template_chain:
-                args.custom_template_chain = chain_info[0]
-
-            template = {}
-            cif_str = get_mmcif(
-                args.custom_template,
-                "custom",
-                args.custom_template_chain,
-                1,
-                len(input_sequence),
-            )
-
-            template["mmcif"] = cif_str
-            template_seq = extract_sequence_from_mmcif(StringIO(cif_str))
-            query_indices, template_indices = align_and_map(
-                input_sequence, template_seq
-            )
-
-            template["queryIndices"] = query_indices
-            template["templateIndices"] = template_indices
-
-            # Add the custom template to the start of the templates list
-            templates.insert(0, template)
-
-            # Add template to the json
-            sequence["protein"]["templates"] = templates
-
-    # Save the output json
-    if args.output_json:
-        with open(args.output_json, "w") as f:
-            json.dump(af3_json, f)
-    else:
-        output_json = args.input_json.replace(".json", "_custom_template.json")
-        with open(output_json, "w") as f:
-            json.dump(af3_json, f)
+    run_custom_template(
+        args.input_json,
+        args.target_id,
+        args.custom_template,
+        args.custom_template_chain,
+        output_json=args.output_json,
+        to_file=True,
+    )
