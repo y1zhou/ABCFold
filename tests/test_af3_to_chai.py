90bcb414ff5fe4b4225405a6d210fa8015316392
hllelli2 <hllelli2@liverpool.ac.uk>
2025-01-03 13:39:48 +0000

    Refactor test_af3_to_chai.py to fix the test with changed test data

--- tests/test_af3_to_chai.py
+++ tests/test_af3_to_chai.py
@@ -14,31 +14,21 @@ def test_af3_to_chai(test_data):
         chai_fasta.json_to_fasta(test_data.test_inputAB_json)
 
         reference = """>protein|A
-GMRESYANENQFGFKTINSDIHKIVIVGGYGKLGGLFARYLRASGYPISILDREDWAVAESILANADVV\
-IVSVPINLTLETIERLKPYLTENMLLADLTSVKREPLAKMLEVHTGAVLGLHPMFGADIASMAKQVVVR\
-CDGRFPERYEWLLEQIQIWGAKIYQTNATEHDHNMTYIQALRHFSTFANGLHLSKQPINLANLLALSSP\
-IYRLELAMIGRLFAQDAELYADIIMDKSENLAVIETLKQTYDEALTFFENNDRQGFIDAFHKVRDWFGD\
-YSEQFLKESRQLLQQANDLKQG
+GMRES
 >protein|B
-GMRESYANENQFGFKTINSDIHKIVIVGGYGKLGGLFARYLRASGYPISILDREDWAVAESILANADVV\
-IVSVPINLTLETIERLKPYLTENMLLADLTSVKREPLAKMLEVHTGAVLGLHPMFGADIASMAKQVVVR\
-CDGRFPERYEWLLEQIQIWGAKIYQTNATEHDHNMTYIQALRHFSTFANGLHLSKQPINLANLLALSSP\
-IYRLELAMIGRLFAQDAELYADIIMDKSENLAVIETLKQTYDEALTFFENNDRQGFIDAFHKVRDWFGD\
-YSEQFLKESRQLLQQANDLKQG
+GMRES
 >protein|C
-GMRESYANENQFGFKTINSDIHKIVIVGGYGKLGGLFARYLRASGYPISILDREDWAVAESILANADVV\
-IVSVPINLTLETIERLKPYLTENMLLADLTSVKREPLAKMLEVHTGAVLGLHPMFGADIASMAKQVVVR\
-CDGRFPERYEWLLEQIQIWGAKIYQTNATEHDHNMTYIQALRHFSTFANGLHLSKQPINLANLLALSSP\
-IYRLELAMIGRLFAQDAELYADIIMDKSENLAVIETLKQTYDEALTFFENNDRQGFIDAFHKVRDWFGD\
-YSEQFLKESRQLLQQANDLKQG
+YANEN
+>ligand|F
+CC(=O)OC1C[NH+]2CCC1CC2
 """
 
         filename = Path(temp_dir) / "chai1.fasta"
 
         assert filename.exists()
-        with open(filename, 'r') as f:
+        with open(filename, "r") as f:
             data = f.read()
-
+        print(data)
         assert data == reference
 
 
@@ -53,7 +43,7 @@ def test_af3_to_chai_rna(test_data):
         filename = Path(temp_dir) / "chai1.fasta"
 
         assert filename.exists()
-        with open(filename, 'r') as f:
+        with open(filename, "r") as f:
             data = f.read()
 
         assert data == reference
@@ -70,7 +60,7 @@ def test_af3_to_chai_dna(test_data):
         filename = Path(temp_dir) / "chai1.fasta"
 
         assert filename.exists()
-        with open(filename, 'r') as f:
+        with open(filename, "r") as f:
             data = f.read()
 
         assert data == reference
@@ -111,7 +101,7 @@ CCCCCCCCCCCC(O)=O
         filename = Path(temp_dir) / "chai1.fasta"
 
         assert filename.exists()
-        with open(filename, 'r') as f:
+        with open(filename, "r") as f:
             data = f.read()
 
         assert data == reference
@@ -130,25 +120,30 @@ def test_chai_output_constraints(test_data):
         df = pd.read_csv(filename)
 
         assert list(df.columns) == [
-            'chainA', 'res_idxA',
-            'chainB', 'res_idxB',
-            'connection_type', 'confidence',
-            'min_distance_angstrom', 'max_distance_angstrom',
-            'comment', 'restraint_id'
-            ]
+            "chainA",
+            "res_idxA",
+            "chainB",
+            "res_idxB",
+            "connection_type",
+            "confidence",
+            "min_distance_angstrom",
+            "max_distance_angstrom",
+            "comment",
+            "restraint_id",
+        ]
 
         assert len(df) == 2
 
-        assert df.iloc[0]['chainA'] == "A"
-        assert df.iloc[0]['res_idxA'] == "C387"
-        assert df.iloc[0]['chainB'] == "B"
-        assert df.iloc[0]['res_idxB'] == "Y101"
-        assert df.iloc[0]['connection_type'] == "contact"
-        assert df.iloc[0]['confidence'] == 1.0
-        assert df.iloc[0]['min_distance_angstrom'] == 0.0
-        assert df.iloc[0]['max_distance_angstrom'] == 5.5
-        assert df.iloc[0]['comment'] == "No comment"
-        assert df.iloc[0]['restraint_id'] == "restraint_0"
+        assert df.iloc[0]["chainA"] == "A"
+        assert df.iloc[0]["res_idxA"] == "C387"
+        assert df.iloc[0]["chainB"] == "B"
+        assert df.iloc[0]["res_idxB"] == "Y101"
+        assert df.iloc[0]["connection_type"] == "contact"
+        assert df.iloc[0]["confidence"] == 1.0
+        assert df.iloc[0]["min_distance_angstrom"] == 0.0
+        assert df.iloc[0]["max_distance_angstrom"] == 5.5
+        assert df.iloc[0]["comment"] == "No comment"
+        assert df.iloc[0]["restraint_id"] == "restraint_0"
 
 
 def test_chai_output_msa(test_data):
@@ -158,8 +153,11 @@ def test_chai_output_msa(test_data):
 
         chai_fasta.json_to_fasta(test_data.test_inputAmsa_json)
 
-        filename = Path(temp_dir) / "1573f21c6a9eb5fb0727d8661eef242d8c09a481f\
+        filename = (
+            Path(temp_dir)
+            / "1573f21c6a9eb5fb0727d8661eef242d8c09a481f\
 c008b01ba22596b3bf31151.aligned.pqt"
+        )
 
         assert filename.exists()
 
@@ -170,14 +168,17 @@ c008b01ba22596b3bf31151.aligned.pqt"
 
         assert len(df) == 4256
 
-        assert df.iloc[0]['sequence'] == "GMRESYANENQFGFKTINSDIHKIVIVGGYGKLGGL\
+        assert (
+            df.iloc[0]["sequence"]
+            == "GMRESYANENQFGFKTINSDIHKIVIVGGYGKLGGL\
 FARYLRASGYPISILDREDWAVAESILANADVVIVSVPINLTLETIERLKPYLTENMLLADLTSVKREPLAKMLEVHT\
 GAVLGLHPMFGADIASMAKQVVVRCDGRFPERYEWLLEQIQIWGAKIYQTNATEHDHNMTYIQALRHFSTFANGLHLS\
 KQPINLANLLALSSPIYRLELAMIGRLFAQDAELYADIIMDKSENLAVIETLKQTYDEALTFFENNDRQGFIDAFHKV\
 RDWFGDYSEQFLKESRQLLQQANDLKQG"
-        assert df.iloc[0]['source_database'] == "query"
-        assert df.iloc[0]['pairing_key'] == ""
-        assert df.iloc[0]['comment'] == "101"
+        )
+        assert df.iloc[0]["source_database"] == "query"
+        assert df.iloc[0]["pairing_key"] == ""
+        assert df.iloc[0]["comment"] == "101"
 
 
 def test_ccd_to_smiles():
