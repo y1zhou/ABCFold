423af74091ff1415f140600ebea7656671c8cfa5
hllelli2 <hllelli2@liverpool.ac.uk>
2025-01-13 11:02:43 +0000

    Added numpy impoty to cope with on the fly pip installs

--- abcfold/chai1/af3_to_chai.py
+++ abcfold/chai1/af3_to_chai.py
@@ -5,6 +5,7 @@ import tempfile
 from pathlib import Path
 from typing import Dict, Optional, Union
 
+import numpy as _  # noqa F401
 import pandas as pd
 import requests
 
@@ -25,8 +26,8 @@ class ChaiFasta:
 
     def __init__(self, working_dir: Union[str, Path]):
         self.working_dir = working_dir
-        self.fasta = Path(working_dir) / 'chai1.fasta'
-        self.constraints = Path(working_dir) / 'chai1_constraints.csv'
+        self.fasta = Path(working_dir) / "chai1.fasta"
+        self.constraints = Path(working_dir) / "chai1_constraints.csv"
         self.msa_file: Optional[Union[str, Path]] = None
 
     def bonded_pairs_to_file(self, bonded_pairs, fasta_data: dict):
@@ -42,10 +43,16 @@ class ChaiFasta:
         """
 
         constraints_headers = [
-            "chainA", "res_idxA", "chainB", "res_idxB",
-            "connection_type", "confidence",
-            "min_distance_angstrom", "max_distance_angstrom",
-            "comment", "restraint_id"
+            "chainA",
+            "res_idxA",
+            "chainB",
+            "res_idxB",
+            "connection_type",
+            "confidence",
+            "min_distance_angstrom",
+            "max_distance_angstrom",
+            "comment",
+            "restraint_id",
         ]
         df = pd.DataFrame(columns=constraints_headers)
 
@@ -62,14 +69,15 @@ class ChaiFasta:
                 pair_data[1],
                 pair_data[2],
                 pair_data[3],
-                'contact',
+                "contact",
                 1.0,
                 0.0,
                 5.5,
-                'No comment',
-                f'restraint_{i}']
+                "No comment",
+                f"restraint_{i}",
+            ]
             df.loc[i] = row_data
-        df.to_csv(self.constraints, sep=',', index=False)
+        df.to_csv(self.constraints, sep=",", index=False)
 
     def msa_to_file(self, msa: str, file_path: Union[str, Path]):
         """
@@ -84,12 +92,12 @@ class ChaiFasta:
         """
 
         # Convert msa to CHAI-1 format with additional MSA source information
-        with tempfile.NamedTemporaryFile(suffix='.a3m', mode='w') as f:
+        with tempfile.NamedTemporaryFile(suffix=".a3m", mode="w") as f:
             f.write(msa)
             f.flush()
             df = merge_multi_a3m_to_aligned_dataframe(
                 msa_a3m_files={Path(f.name): MSADataSource.UNIPROT},
-                insert_keys_for_sources='uniprot'
+                insert_keys_for_sources="uniprot",
             )
 
         # Write the dataframe to a parquet file
@@ -115,87 +123,73 @@ class ChaiFasta:
             json_dict = json_file_or_dict
 
         fasta_data: Dict[str, str] = {}
-        with open(self.fasta, 'w') as f:
-            for seq in json_dict['sequences']:
-                if 'protein' in seq:
-                    protein_str, fasta_data = self.add_protein(
-                        seq,
-                        fasta_data
-                        )
+        with open(self.fasta, "w") as f:
+            for seq in json_dict["sequences"]:
+                if "protein" in seq:
+                    protein_str, fasta_data = self.add_protein(seq, fasta_data)
                     f.write(protein_str)
-                if 'rna' in seq:
+                if "rna" in seq:
                     nucleotide_str, fasta_data = self.add_nucleotide(
-                        seq,
-                        'rna',
-                        fasta_data
-                        )
+                        seq, "rna", fasta_data
+                    )
                     f.write(nucleotide_str)
-                if 'dna' in seq:
+                if "dna" in seq:
                     nucleotide_str, fasta_data = self.add_nucleotide(
-                        seq,
-                        'dna',
-                        fasta_data
-                        )
+                        seq, "dna", fasta_data
+                    )
                     f.write(nucleotide_str)
-                if 'ligand' in seq:
+                if "ligand" in seq:
                     ligand_str = self.add_ligand(seq)
                     f.write(ligand_str)
 
         # Check if there are bonded pairs
-        if 'bondedAtomPairs' in json_dict.keys():
-            bonded_pairs = json_dict['bondedAtomPairs']
+        if "bondedAtomPairs" in json_dict.keys():
+            bonded_pairs = json_dict["bondedAtomPairs"]
             self.bonded_pairs_to_file(bonded_pairs, fasta_data)
 
     def add_protein(self, seq: dict, fasta_data: dict):
-        prot_id = seq['protein']['id']
+        prot_id = seq["protein"]["id"]
         if isinstance(prot_id, list):
             protein_str = ""
-            for i in seq['protein']['id']:
-                temp_prot_str, fasta_data = self._add_protein(
-                    seq,
-                    i,
-                    fasta_data)
+            for i in seq["protein"]["id"]:
+                temp_prot_str, fasta_data = self._add_protein(seq, i, fasta_data)
                 protein_str += temp_prot_str
         else:
-            protein_str, fasta_data = self._add_protein(seq,
-                                                        prot_id,
-                                                        fasta_data)
+            protein_str, fasta_data = self._add_protein(seq, prot_id, fasta_data)
         return protein_str, fasta_data
 
     def _add_protein(self, seq: dict, prot_id: str, fasta_data: dict):
         protein_str = f">protein|{prot_id}\n{seq['protein']['sequence']}\n"
-        sequence = seq['protein']['sequence']
-        if 'unpairedMsa' in seq['protein'].keys():
+        sequence = seq["protein"]["sequence"]
+        if "unpairedMsa" in seq["protein"].keys():
             seq_hash = hashlib.sha256(sequence.upper().encode()).hexdigest()
-            pqt_path = Path(self.working_dir) / f'{seq_hash}.aligned.pqt'
-            msa = seq['protein']['unpairedMsa']
+            pqt_path = Path(self.working_dir) / f"{seq_hash}.aligned.pqt"
+            msa = seq["protein"]["unpairedMsa"]
             self.msa_to_file(msa=msa, file_path=pqt_path)
         fasta_data[prot_id] = sequence
 
         return protein_str, fasta_data
 
     def add_nucleotide(self, seq: dict, seq_type: str, fasta_data: dict):
-        nucl_id = seq[seq_type]['id']
+        nucl_id = seq[seq_type]["id"]
 
         if isinstance(nucl_id, list):
             nucleotide_str = ""
-            for i in seq[seq_type]['id']:
-                temp_nucl_str, fasta_data = self._add_nucleotide(seq,
-                                                                 seq_type,
-                                                                 i,
-                                                                 fasta_data)
+            for i in seq[seq_type]["id"]:
+                temp_nucl_str, fasta_data = self._add_nucleotide(
+                    seq, seq_type, i, fasta_data
+                )
                 nucleotide_str += temp_nucl_str
         else:
-            nucleotide_str, fasta_data = self._add_nucleotide(seq,
-                                                              seq_type,
-                                                              nucl_id,
-                                                              fasta_data)
+            nucleotide_str, fasta_data = self._add_nucleotide(
+                seq, seq_type, nucl_id, fasta_data
+            )
 
         return nucleotide_str, fasta_data
 
     def _add_nucleotide(self, seq: dict, seq_type: str, nucl_id: str, fasta_data: dict):
         nucleotide_str = f">{seq_type}|{nucl_id}\n{seq[seq_type]['sequence']}\n"
-        fasta_data[nucl_id] = seq[seq_type]['sequence']
+        fasta_data[nucl_id] = seq[seq_type]["sequence"]
         return nucleotide_str, fasta_data
 
     def ccd_to_smiles(self, ccd_id: str):
@@ -209,21 +203,21 @@ class ChaiFasta:
             return None
 
     def add_ligand(self, seq: dict):
-        lig_id = seq['ligand']['id']
+        lig_id = seq["ligand"]["id"]
         ligand_str = ""
-        if 'ccdCode' in seq['ligand']:
+        if "ccdCode" in seq["ligand"]:
             if isinstance(lig_id, list):
-                for i in seq['ligand']['id']:
-                    smile = self.ccd_to_smiles(seq['ligand']['ccdCode'][0])
+                for i in seq["ligand"]["id"]:
+                    smile = self.ccd_to_smiles(seq["ligand"]["ccdCode"][0])
                     if smile:
                         ligand_str += f">ligand|{i}\n{smile}\n"
             else:
-                smile = self.ccd_to_smiles(seq['ligand']['ccdCode'])
+                smile = self.ccd_to_smiles(seq["ligand"]["ccdCode"])
                 if smile:
                     ligand_str = f">ligand|{lig_id}\n{smile}\n"
-        if 'smiles' in seq['ligand']:
+        if "smiles" in seq["ligand"]:
             if isinstance(lig_id, list):
-                for i in seq['ligand']['id']:
+                for i in seq["ligand"]["id"]:
                     ligand_str += f">ligand|{i}\n{seq['ligand']['smiles']}\n"
             else:
                 ligand_str = f">ligand|{lig_id}\n{seq['ligand']['smiles']}\n"
